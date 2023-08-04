1b743e38f8440b8fce8b3ae3f7f5e7e12333922d
Salih <salihyildirimce@gmail.com>
2023-08-04 16:21:04 +0300

    thread

---
+++ src/GenericExamples/Main.java
@@ -0,0 +1,46 @@
+package GenericExamples;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String []args){
+//        YourCustomer<Integer,String > yourCustomer= new YourCustomer<>();
+//
+//        //System.out.println(yourCustomer.x.getClass().getTypeName());
+//       // yourCustomer.myFons();
+//        Integer[] myLists = {5,6,7};
+//        String myFoo = yourCustomer.myFoo(myLists, "ok", "ekmek");
+//        System.out.println(myFoo);
+//        System.out.println("---------------------------");
+//        ArrayList<Integer> arr = new ArrayList<>();
+//        arr.add(1);
+//        arr.add(2);
+//        arr.add(3);
+//
+//        // Store the original size
+//        int originalSize = arr.size();
+//
+//        // Add or remove elements from the ArrayList
+//        arr.add(4);
+//        arr.add(5);
+//        arr.remove(0);
+//
+//        // See the original size
+//        System.out.println("Original size of ArrayList: " + originalSize);
+//        System.out.println("Original size of ArrayList: " + Stream.);
+
+        HashSet<Integer> hashSet= new HashSet<>();
+
+        hashSet.add(5);
+        hashSet.add(4);
+        hashSet.add(1);
+        hashSet.add(6);
+        hashSet.add(9);
+
+
+  }
+}
--- src/GenericExamples/YourCustomer.java
+++ src/GenericExamples/YourCustomer.java
@@ -1,4 +1,43 @@
 package GenericExamples;
 
-public class YourCustomer {
+import com.sun.security.auth.NTNumericCredential;
+
+import javax.xml.transform.stream.StreamSource;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class YourCustomer<T,C> { // diger sınıflarla bir ilgisi olmayan tek basina ornektir.
+    public T x;
+    public String y ;
+
+    public void myFons() {
+        System.out.println(y.getClass().getName());
+        System.out.println("Deneme" + x);
+        System.out.println(x.getClass().getName());
+    }
+
+    public  <T,K> C myFoo(T[] t,K k,C c){
+
+        List<String> myList = new LinkedList<>();
+        Stream.of(t).forEach(System.out::println);
+        Stream.of(myList).forEach(System.out::println);
+        System.out.println(k);
+        Arrays.stream(t).forEach(System.out::println);
+
+        List<List<String>> matrix= new LinkedList<>();
+
+
+
+
+        return c;
+    }
+    public void arraylenght(){
+
+        ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        System.out.println(arr2.stream().distinct());
+
+    }
 }
